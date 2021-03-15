fe37fce45a31f2123ab7d14ea8abacbe17aadd0a
Masahiro Yasumoto <yasumoto@araya.org>
2021-03-15 19:40:13 +0900

    レビュー欄が開閉できるようにする

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,9 @@
 import $ from 'jquery'
 import createBookListItem from './createBookListItem'
 import createBookReview from './createBookReview'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Timeline from './Timeline'
 
 export type Book = {
   id: number
@@ -18,39 +21,42 @@ export type Review = {
   like: number
 }
 
-$(function() {
-  $.ajax('http://localhost:1323/books')
-    .done(function(books: Book[]) {
-      books.forEach((book: Book)=> $('#js-book-list').append($(createBookListItem(book))))
+const root = document.getElementById('react-root')
+ReactDOM.render(<Timeline />, root)
 
-      $('.js-toggle-review').on('click', function(event) {
-        var bookId = $(this).data('bookId')
-        $('.js-review[data-book-id="' + bookId + '"]').toggle('fast')
+// $(function() {
+//   $.ajax('http://localhost:1323/books')
+//     .done(function(books: Book[]) {
+//       books.forEach((book: Book)=> $('#js-book-list').append($(createBookListItem(book))))
 
-        return false
-      })
+//       $('.js-toggle-review').on('click', function(event) {
+//         var bookId = $(this).data('bookId')
+//         $('.js-review[data-book-id="' + bookId + '"]').toggle('fast')
 
-      $(document).on('click', '.js-like', function(event) {
-        var likeCountElement = $(this).find('.js-like-count')
-        likeCountElement.text(likeCountElement.text() + 1)
+//         return false
+//       })
 
-        return false
-      })
+//       $(document).on('click', '.js-like', function(event) {
+//         var likeCountElement = $(this).find('.js-like-count')
+//         likeCountElement.text(likeCountElement.text() + 1)
 
-      $(document).on('submit', '.js-form', function(event) {
-        var bookId = $(this).data('bookId')
-        $.ajax({
-          url: 'http://localhost:1323/reviews',
-          type: 'post',
-          dataType: 'json',
-          data: {
-            comment: $(event.currentTarget).find('textarea').val()
-          }
-        }).done(function(review) {
-          $('.js-review[data-book-id="' + bookId + '"] > ul').append($(createBookReview(review)))
-        })
+//         return false
+//       })
 
-        return false
-      })
-    })
-})
+//       $(document).on('submit', '.js-form', function(event) {
+//         var bookId = $(this).data('bookId')
+//         $.ajax({
+//           url: 'http://localhost:1323/reviews',
+//           type: 'post',
+//           dataType: 'json',
+//           data: {
+//             comment: $(event.currentTarget).find('textarea').val()
+//           }
+//         }).done(function(review) {
+//           $('.js-review[data-book-id="' + bookId + '"] > ul').append($(createBookReview(review)))
+//         })
+
+//         return false
+//       })
+//     })
+// })
