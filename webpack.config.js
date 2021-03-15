932763a5656d3dba6407ab9e125700841a690ab9
Masahiro Yasumoto <yasumoto@araya.org>
2021-03-15 15:04:29 +0900

    complete chapter 6-3

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    mode: process.env.NODE_ENV || 'development',
+    entry: './src/app.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                // .jsもしくは.tsを対象とする
+                test: /\.(j|t)s$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+};
